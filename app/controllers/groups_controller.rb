f72bfbf294d21ce6a9976ad84c0965eb1cf25a46
masatechexpert <cate35cate@gmail.com>
2019-05-29 13:22:39 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,36 +1,39 @@
-class MessagesController < ApplicationController
-  before_action :set_group
+class GroupsController < ApplicationController
+   before_action :set_group, only: [:edit, :update]
 
   def index
-    @message = Message.new
-    @messages = @group.messages.includes(:user)
-      respond_to do |format|
-        format.html
-        format.json
-      end
+
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
-    @message = @group.messages.new(message_params)
-    if @message.save
-      respond_to do |format|
-        format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
-        format.json
-      end
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
     else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+      render :new
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
     end
   end
 
   private
-
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
   end
 
-  def set_group
-    @group = Group.find(params[:group_id])
+   def set_group
+    @group = Group.find(params[:id])
   end
+
 end
